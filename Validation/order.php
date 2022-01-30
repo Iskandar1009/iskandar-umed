8059f85ae4f99869d3a34c03299abd6dac7c50a8
Iskandar1009 <93070214+Iskandar1009@users.noreply.github.com>
2022-01-30 13:30:59 +0500

    correct errors

--- Validation/order.php
+++ Validation/order.php
@@ -1,18 +1,24 @@
 <?php
 
-function typeChecking($str){
-    //CODE BY ISKANDAR
-    if((int) $str == 0){
-        return "Variable's type is true. ";
+function validateString($order)
+{
+    if((int) $order != 0){
+        return "ERROR 422, type of order have to be string. ";
     }
-    return "ERROR, invalid type of variable. ";
+    return "Type of order is ok. ";
 }
-function order(string $order){
-    //CODE BY ISKANDAR
-    $orders = ["Рифы", "Земля", "Тонем"];
-    if(strlen($order) > 15 || strlen($order) < 2){
-        return "ERROR, your order is long or short.";
+function minMaxChecking(string $order){
+    $min = 2;
+    $max = 15;
+    if (strlen($order) >= $max) {
+        return "A lot of characters in the order. ";
     }
+    if (strlen($order) < $min) {
+        return "Very few characters in the order. ";
+    }
+    return "Number of characters are ok. ";
+}
+function searchInList(string $order, array $orders){
     for($i = 0; $i <= 3; $i++){
         if($order == $orders[$i]){
             return "This order is on your order list.";
@@ -20,6 +26,14 @@ function order(string $order){
     }
     return "This order is not in your list of orders.";
 }
+function order(string $order){
+    //CODE BY ISKANDAR
+    $orders = ["Рифы", "Земля", "Тонем"];
+    $type_checking_str = validateString($order);
+    $min_max_checking = minMaxChecking($order);
+    $search_in_list = searchInList($order, $orders);
+    return "$type_checking_str $min_max_checking $search_in_list";
+    
+}
 $order = readline("Input order: ");
-echo typeChecking($order);
 echo order($order);
