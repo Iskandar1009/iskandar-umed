929fe3d18e218492fb4741c33846e810069d2491
UmedEminov <umedeminov@gmail.com>
2022-01-30 08:57:46 +0500

    realize fishermans order

---
+++ Validation/fishermans_order.php
@@ -0,0 +1,67 @@
+<?php
+
+function typeChecking($str, $int)
+{
+    if((int) $str == 0 && (int) $int != 0){
+        return "Variable's type is true. ";
+    }
+    return "ERROR, invalid type of variable. ";
+}
+
+function orderFromFisherman(string $fish_name, string $fish_number)
+{
+    //проверить типы данных
+    //длина команды минимум и максимум
+    //находится для в наборе уже существующих рыб в океане
+    $new_fish_number = (int) $fish_number;
+    $fishes = [
+        "Окунь",
+        "Карась",
+        "Сазан",
+        "Лосось"
+    ];
+
+    if (strlen($fish_name) >= 15) {
+        return "A lot of characters in the name of the fish";
+    }
+    if (strlen($fish_name) < 3) {
+        return "Very few characters in the name of the fish";
+    }
+    if ($fishes[0] == $fish_name){
+        return "This fish is on the list";
+    }
+    if ($fishes[1] == $fish_name){
+        return "This fish is on the list";
+    }
+    if ($fishes[2] == $fish_name){
+        return "This fish is on the list";
+    }
+    if ($fishes[3] == $fish_name){
+        return "This fish is on the list";
+    } else {
+        return "ERROR - 422!Change your order!";
+    }
+} 
+    $fish_name = readline("Input fish's name: ");
+    $fish_number = readline("Input number of fishes: ");
+    echo typeChecking($fish_name, $fish_number);
+    echo orderFromFisherman($fish_name, $fish_number);
+    
+    
+    /*echo " ";
+    echo "is_string(";
+    var_export($fish_name);
+    echo ") = ";
+    var_dump(is_string($fish_name));
+
+    echo "is_int(";
+    var_export($fish_number);
+    echo ") = ";
+    var_dump(is_string($fish_number)); */
+
+
+
+    /*echo "<br>";
+    echo gettype($fish_name);
+    echo "<br>";
+    echo gettype($fish_number);*/
