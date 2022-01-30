8108a687e79aff3c3ec2bffaf0016eac703ad43f
Iskandar1009 <iskandarismoili@gmail.com>
2022-01-30 22:34:52 +0500

    did format

--- Validation/order.php
+++ Validation/order.php
@@ -2,43 +2,47 @@
 
 function validateString($item)
 {
-    if(!is_string($item)){
+    if (!is_string($item)) {
         return "ERROR 422, type of order have to be string. ";
     }
     return "Type of order is ok. ";
 }
-function validateMin($item, $min_value){
+function validateMin($item, $min_value)
+{
     if (strlen($item) <= $min_value) {
         return "Very few characters in the order. ";
     }
     return "Min number of characters are ok. ";
 }
-function validateMax($item, $max_value){
+function validateMax($item, $max_value)
+{
     if (strlen($item) >= $max_value) {
         return "A lot of characters in the order. ";
     }
-    
+
     return "Max number of characters are ok. ";
 }
-function validateInArray($order, $orders){
-    if(!in_array($order, $orders)){
+function validateInArray($order, $orders)
+{
+    if (!in_array($order, $orders)) {
         return "This order isn't listed. ";
     }
     return "This order is listed. ";
 }
-function order($order){
+function order($order)
+{
     $validation_message = "";
     $validation_message .= validateString($order) . '</br>';
     $validation_message .= validateMin($order, 2) . '</br>';
     $validation_message .= validateMax($order, 15) . '</br>';
-    $orders = 
-    ["Рифы", 
-    "Земля", 
-    "Тонем"
-];
+    $orders =
+        [
+            "Рифы",
+            "Земля",
+            "Тонем"
+        ];
     $validation_message .= validateInArray($order, $orders) . '</br>';
     return $validation_message;
-    
 }
 $order = readline("Input order: ");
 echo order($order);
