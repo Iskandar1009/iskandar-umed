078c2825ddb10f8afdb4a4f6d7d4f8270e5aa9d7
UmedEminov <umedeminov@gmail.com>
2022-01-31 12:08:58 +0500

    correct errors

--- Validation/fishermans_order.php
+++ Validation/fishermans_order.php
@@ -1,67 +1,60 @@
 <?php
 
-function typeChecking($str, $int)
+function validateString($fish_name)
 {
-    if((int) $str == 0 && (int) $int != 0){
-        return "Variable's type is true. ";
+    if(!is_string($fish_name)){
+        return "ERROR 422, type of fish name have to be string. ";
     }
-    return "ERROR, invalid type of variable. ";
+    return "Type of fish name is ok. ";
+}
+function validateInt($fish_number)
+{
+    if(!is_int($fish_number)){
+        return "ERROR 422, type of fish number have to be int. ";
+    }
+    return "Type of fish number is ok. ";
 }
 
-function orderFromFisherman(string $fish_name, string $fish_number)
+function validateMin($fish_name, $min_value)
+{
+    if (strlen($fish_name) <= $min_value) {
+        return "Very few characters in the name of the fish. ";
+    }
+    return "Number of characters are ok. ";
+}
+function validateMax($fish_name, $max_value)
+{
+    if (strlen($fish_name) >= $max_value) {
+        return "A lot of characters in the name of the fish. ";
+    }
+    return "Number of characters are ok. ";
+}
+
+function validateInArray(string $fish_name, array $fishes)
+{
+    if(!in_array($fish_name, $fishes)){
+        return "This fish isn't listed. ";
+    }
+    return "This fish is listed. ";
+}
+function orderFishes(string $fish_name, string $fish_number)
 {
     //проверить типы данных
     //длина команды минимум и максимум
     //находится для в наборе уже существующих рыб в океане
-    $new_fish_number = (int) $fish_number;
     $fishes = [
         "Окунь",
         "Карась",
         "Сазан",
         "Лосось"
     ];
-
-    if (strlen($fish_name) >= 15) {
-        return "A lot of characters in the name of the fish";
-    }
-    if (strlen($fish_name) < 3) {
-        return "Very few characters in the name of the fish";
-    }
-    if ($fishes[0] == $fish_name){
-        return "This fish is on the list";
-    }
-    if ($fishes[1] == $fish_name){
-        return "This fish is on the list";
-    }
-    if ($fishes[2] == $fish_name){
-        return "This fish is on the list";
-    }
-    if ($fishes[3] == $fish_name){
-        return "This fish is on the list";
-    } else {
-        return "ERROR - 422!Change your order!";
-    }
+    $validate_string = validateString($fish_name) . '</br>';
+    $validate_int = validateInt($fish_number) . '</br>';
+    $validate_min = validateMin($fish_name, 2) . '</br>';
+    $validate_max = validateMax($fish_name, 15). '</br>';
+    $validate_in_array = validateInArray($fish_name, $fishes) . '</br>';
+    return "$validate_string $validate_int $validate_min $validate_max $validate_in_array";
 } 
     $fish_name = readline("Input fish's name: ");
     $fish_number = readline("Input number of fishes: ");
-    echo typeChecking($fish_name, $fish_number);
-    echo orderFromFisherman($fish_name, $fish_number);
-    
-    
-    /*echo " ";
-    echo "is_string(";
-    var_export($fish_name);
-    echo ") = ";
-    var_dump(is_string($fish_name));
-
-    echo "is_int(";
-    var_export($fish_number);
-    echo ") = ";
-    var_dump(is_string($fish_number)); */
-
-
-
-    /*echo "<br>";
-    echo gettype($fish_name);
-    echo "<br>";
-    echo gettype($fish_number);*/
+    echo orderFishes($fish_name, $fish_number);
