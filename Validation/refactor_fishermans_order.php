3eec871d2b20e7718390cda84409733b0f0754b1
Umed <umedeminov@gmail.com>
2022-02-05 17:06:23 +0500

    refactoring is ready

--- Validation/refactor_fishermans_order.php
+++ Validation/refactor_fishermans_order.php
@@ -1,48 +1,54 @@
 <?php
-
-function validateString($fish_name)
+function validateString($fish_name, $fish_number = null, $min_value = null, $max_value = null, $available_fishes = null)
 {
-    if (is_string($fish_name)) {
-        return "Type of fish name is ok. ";
+    if (!is_string($fish_name)) {
+        return "ERROR 422, type of fish name have to be string.";
     }
-    return "ERROR 422, type of fish name have to be string. ";
+    return "Type of fish name is ok.";
 }
-function validateInt($fish_number)
+function validateInt($fish_name = null, $fish_number, $min_value = null, $max_value = null, $available_fishes = null)
 {
-    if (is_int($fish_number)) {
-        return "Type of fish number is ok. ";
+    if (!is_int($fish_number)) {
+        return "ERROR 422, type of fish number have to be int.";
     }
-    return "ERROR 422, type of fish number have to be int. ";
+    return "Type of fish number is ok.";
 }
-function validateMin($fish_name, $min_value)
+function validateMin($fish_name, $fish_number = null, $min_value, $max_value = null, $available_fishes = null)
 {
     if (strlen($fish_name) <= $min_value) {
         return "Very few characters in the name of the fish. ";
     }
     return "Min number of characters are ok. ";
 }
-function validateMax($fish_name, $max_value)
+function validateMax($fish_name, $fish_number = null, $min_value = null, $max_value, $available_fishes = null)
 {
     if (strlen($fish_name) >= $max_value) {
         return "A lot of characters in the name of the fish. ";
     }
     return "Max number of characters are ok. ";
 }
-function validateInArray($fish_name, $available_fishes)
+function validateInArray($fish_name, $fish_number = null, $min_value = null, $max_value = null, $available_fishes)
 {
-    if (in_array($fish_name, $available_fishes)){
-        return "This fish is listed. ";
+    if (!in_array($fish_name, $available_fishes)){
+        return "This fish isn't listed. ";
     }
-    return "This fish isn't listed. ";
+    return "This fish is listed. ";
 }
-function validate($fish_name, $rules, $fish_number, $available_fishes)
+function validate($fish_order, $fish_number, $rules, $available_fishes)
 {
     $messages = [];
-    $rules_in_array = explode('|', $rules);
-    array_push($messages, call_user_func_array("validate", [$fish_name, $rules_in_array[2], $rules_in_array[3],  $available_fishes]));
+    $rules_in_arr = explode('|', $rules);
+    foreach ($rules_in_arr as $rule) {
+        if (stristr($rule, ':') !== FALSE) {
+            $value = explode(':', $rule);
+            array_push($messages, call_user_func_array("validate" . ucfirst($value[0]), [$fish_order, $fish_number, $value[1], $value[1],  $available_fishes]));
+        } else {
+            array_push($messages, call_user_func_array("validate" . ucfirst($rule), [$fish_order, $fish_number, null, null,  $available_fishes]));
+        }
+    }
     return $messages;
 }
-function index($fish_name, $fish_number)
+function index($fish_order, $fish_number)
 {
     $available_fishes = [
         "Карась",
@@ -50,13 +56,12 @@ function index($fish_name, $fish_number)
         "Лосось",
         "Форель"
     ];
-    $rules = 'string|int|min:2|max:20|in:$available_fishes';
-    $messages = validate($fish_name, $rules, $available_fishes);
-
+    $rules = 'string|int|min:2|max:20|inArray:$available_fishes';
+    $messages = validate($fish_order, $fish_number, $rules, $available_fishes);
     return count($messages)
         ? implode (',\n', $messages)
         : 'No validation message';
 }
-    $fish_name = readline("Input fish's name: ");
+    $fish_order = readline("Input fish's name: ");
     $fish_number = readline("Input number of fishes: ");
-    echo index($fish_name, $fish_number);
+    echo index($fish_order, $fish_number);
